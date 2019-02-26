72ae5fa8938c840efaa4874c10ba7fbdacb35d91
Yika Zhang <59418213@qq.com>
2019-02-27 01:47:30 +0800

    Add a timer

--- js/app.js
+++ js/app.js
@@ -44,3 +44,48 @@ $.each(cardShuffle,function(index,value){
  *    + 增加移动计数器并将其显示在页面上（将这个功能放在你从这个函数中调用的另一个函数中）
  *    + 如果所有卡都匹配，则显示带有最终分数的消息（将这个功能放在你从这个函数中调用的另一个函数中）
  */
+
+//计时器（来源：https://www.jb51.net/article/105783.htm)
+ var hour,minute,second;//时 分 秒
+ hour=minute=second=0;//初始化
+ var millisecond=0;//毫秒
+ var int;
+ function Reset()//重置
+ {
+   window.clearInterval(int);
+   millisecond=hour=minute=second=0;
+   document.getElementById('timetext').value='00时00分00秒000毫秒';
+ }
+
+ function start()//开始
+ {
+   int=setInterval(timer,50);
+ }
+
+ function timer()//计时
+ {
+   millisecond=millisecond+50;
+   if(millisecond>=1000)
+   {
+     millisecond=0;
+     second=second+1;
+   }
+   if(second>=60)
+   {
+     second=0;
+     minute=minute+1;
+   }
+
+   if(minute>=60)
+   {
+     minute=0;
+     hour=hour+1;
+   }
+   document.getElementById('timetext').value=hour+'时'+minute+'分'+second+'秒'+millisecond+'毫秒';
+
+ }
+
+ function stop()//暂停
+ {
+   window.clearInterval(int);
+ }
